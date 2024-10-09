787e2c8ca6a656b365703edeefc3a55dec97760a
VL <veranikalis11@gmail.com>
2024-10-09 10:40:24 +0200

    created class Horse.java

---
+++ src/Animals/PackAnimals/Horse.java
@@ -0,0 +1,11 @@
+package Animals.PackAnimals;
+
+import Animals.PackAnimal;
+
+import java.time.LocalDate;
+
+public class Horse extends PackAnimal {
+    public Horse(String name, String commands, LocalDate birthDate) {
+        super(name, commands, birthDate);
+    }
+}
