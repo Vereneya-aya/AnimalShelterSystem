a1a7a6c1c3a47d5a8c7344201566160a0b2dc7ba
VL <veranikalis11@gmail.com>
2024-10-09 10:28:19 +0200

    created abstract class PackAnimal

---
+++ src/AnimalShelter.java
@@ -0,0 +1,16 @@
+import java.time.LocalDate;
+
+public abstract class Animal {
+    protected String name;
+    protected String commands;
+    protected LocalDate birthDate;
+
+    public Animal(String name, String commands, LocalDate birthDate) {
+        this.name = name;
+        this.commands = commands;
+        this.birthDate = birthDate;
+    }
+
+    public abstract void addCommand(String command);
+    public abstract void showCommands();
+}
