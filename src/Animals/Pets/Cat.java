51aa9b540dcd9045a8c051f7ce69579da09f1d43
VL <veranikalis11@gmail.com>
2024-10-09 10:38:27 +0200

    created class Cat

---
+++ src/Animals/Pets/Cat.java
@@ -0,0 +1,11 @@
+package Animals.Pets;
+
+import Animals.Pet;
+
+import java.time.LocalDate;
+
+public class Cat extends Pet {
+    public Cat(String name, String commands, LocalDate birthDate) {
+        super(name, commands, birthDate);
+    }
+}
